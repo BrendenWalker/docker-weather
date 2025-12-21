01894659e07112dc881ed95186a8e33f8e9d4005
Fabian Netz <fabian.netz@outlook.de>
2025-12-22 00:15:47 +0100

    changed port config

--- app/app.py
+++ app/app.py
@@ -128,4 +128,4 @@ def datenschutz():
     return render_template("datenschutz.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
